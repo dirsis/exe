903d34ce4b236c24d42786c5b464dab4e3e98fd4
dirsis <dirsis@gmail.com>
2025-04-10 19:41:00 -0300

    04_10_2025

--- commit.bat
+++ commit.bat
@@ -1,3 +1,8 @@
+   for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
+     set day=%%i
+     set month=%%j
+     set year=%%k
+   )
 set datestr=%month%_%day%_%year%
 git add *
 git commit -m "%datestr%"
